773f3cb542bfc45d2ecdea31bbdf684b2e2f3348
Rakesh KK <kkrakeshkumar0306@gmail.com>
2023-05-09 19:06:16 +0530

    mobile-menu changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 
 export const Menu = () => {
-  const [menuItem,setMenuItem]=useState('')
+  const [menuitem,setMenuItem]=useState('')
   const [isMobileMenu,setIsMobileMenu]=useState(false)
   const [left,setLeft]=useState(-150)
   const getBrowserWidth=()=>{
@@ -46,16 +46,17 @@ export const Menu = () => {
   }
    return (
     <div className='menu'>
-        <ul>
-
-      <li>
-        <Link id="home"  className={menuitem=="home" &&"active-menu"}onClick={fnmenuclick} href="/home">Home</Link>
+      {isMobileMenu && <button onClick={fnMobileMenuBtnClick}className='mobile-menu-btn'>menu</button>}
+        <ul style={{left:left}}className={isMobileMenu?"mobile-menu":"menu"}>
+        <li>
+        {isMobileMenu && <span onClick={fnClose}className='close-btn'>x</span>}
+        <Link id="home"  className={menuitem=="home" &&"active-menu"} onClick={fnMenuClick} href="/home">Home</Link>
       </li>
       <li>
-        <Link  id="about"className={menuitem=="about" &&"active-menu"} onClick={fnmenuclick}href="/about">About Us</Link>
+        <Link  id="about"className={menuitem=="about" &&"active-menu"} onClick={fnMenuClick}href="/about">About Us</Link>
       </li>
       <li>
-        <Link  id="contact" className={menuitem=="contact" &&"active-menu"}onClick={fnmenuclick} href="/contact">contact</Link>
+        <Link  id="contact" className={menuitem=="contact" &&"active-menu"} onClick={fnMenuClick} href="/contact">contact</Link>
 
       </li>
     </ul>
